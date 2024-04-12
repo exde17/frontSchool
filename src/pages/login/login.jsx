3d1f8c2bb4755e1ae6377512504e50a9d05c87aa
AndresMouthon <andresdomingomouthon541@gmail.com>
2024-04-12 10:50:34 -0500

    Estilizando login y registro de usuario

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,49 +1,49 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
-
+import { Link, useNavigate } from "react-router-dom";
+import img from "../../img/Login.png";
+import PersonIcon from "@mui/icons-material/Person";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
 import "./login.css";
-import ram  from "./conexion";
-
+import ram from "./conexion";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState(''); 
-  const [alertMessage, setAlertMessage] = useState('');
-  const [showAlert, setShowAlert] =useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
   // estado de carga cuando se envian datos
-  const [cargando, setCargando] =useState(false);
-  
+  const [cargando, setCargando] = useState(false);
 
   const navigate = useNavigate(); // Obtén navigate aquí
   //javierrodriguez@gmail.com
   //Javierrodriguez1234
   //
 
-    const handleLogin = async (event) => {
-      event.preventDefault();
-      setCargando(true);// activa la ventana para mostrar 'cargando...'
-        try {
-           const response = await ram(email, password, navigate)
-              console.log(response);            
-              
-        } catch (error) {
-            alert('Error en la solicitud', error);
-            console.error('Error en la solicitud:', error);
-            setAlertMessage('Error en las credenciales. Vefifica e intentalo nuevamente');
-     
-            // Reiniciar el formulario
-            setEmail('');
-            setPassword('');
-            
-       }// codigo de ventana 'cargando...'
-       finally {
-        // Independientemente del resultado, deja de mostrar el estado de carga
-        setCargando(false);
-      }
-    };
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setCargando(true); // activa la ventana para mostrar 'cargando...'
+    try {
+      const response = await ram(email, password, navigate);
+      console.log(response);
+    } catch (error) {
+      alert("Error en la solicitud", error);
+      console.error("Error en la solicitud:", error);
+      setAlertMessage(
+        "Error en las credenciales. Vefifica e intentalo nuevamente"
+      );
+
+      // Reiniciar el formulario
+      setEmail("");
+      setPassword("");
+    } finally {
+      // codigo de ventana 'cargando...'
+      // Independientemente del resultado, deja de mostrar el estado de carga
+      setCargando(false);
+    }
+  };
   return (
     <div className="login">
-      {cargando && <p>Cargando...</p>}
+      {cargando && <p className="cargando">Cargando...</p>}
       {showAlert && (
         <div className="floating-alert">
           <span>{alertMessage}</span>
@@ -52,18 +52,48 @@ const Login = () => {
       )}
       <form onSubmit={handleLogin}>
         <div className="container">
-          <h1>ADMIN</h1>
-          <input type="email" placeholder="Email" value={email} 
-              onChange={(e) => setEmail(e.target.value)} required/>
-          <input type="password" placeholder="Password" value={password} 
-              onChange={(e) => setPassword(e.target.value)} required/>
-          <button type='submit'>Login</button>
-          <span>No tienes cuenta? <Link to='/signUp'>Registrate</Link></span>
+          <header className="content-img">
+            <img src={img} />
+          </header>
+          <div className="content-inputs">
+            <fieldset className="inputs">
+              <span className="icons">
+                <PersonIcon />
+              </span>
+              <input
+                type="text"
+                placeholder="Usuario"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </fieldset>
+            <fieldset className="inputs">
+              <span className="icons">
+                <VpnKeyIcon />
+              </span>
+              <input
+                type="password"
+                placeholder="**********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </fieldset>
+            <span className="registro">
+              No tienes cuenta?{" "}
+              <Link to="/signUp" style={{ marginLeft: "5px" }}>
+                Registrate
+              </Link>
+            </span>
+          </div>
         </div>
+        <footer className="btn-iniciar">
+          <button>Iniciar sesion</button>
+        </footer>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
-
+export default Login;
--- src/pages/signUp/LoginUser.jsx
+++ src/pages/signUp/LoginUser.jsx
@@ -1,19 +1,24 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./loginuser.css";
 import registro from "./conexionUser";
+import PersonIcon from "@mui/icons-material/Person";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
+import KeyboardAltIcon from '@mui/icons-material/KeyboardAlt';
+import img from "../../img/RegistrarUsuario.png";
 
 const LoginUser = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');   
-  const [alertMessage, setAlertMessage] = useState('');
-  const [showAlert, setShowAlert] =useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
   // estado de carga cuando se envian datos
-  const [cargando, setCargando] =useState(false);
+  const [cargando, setCargando] = useState(false);
   //Restricciones de la contraseña
-  const [showPasswordRestrictions, setShowPasswordRestrictions] = useState(false);
+  const [showPasswordRestrictions, setShowPasswordRestrictions] =
+    useState(false);
 
   const handleChange = (event) => {
     const newPassword = event.target.value;
@@ -21,39 +26,36 @@ const LoginUser = () => {
   };
 
   const passwordRestrictions = [
-    'Al menos 8 caracteres',
-    'Al menos una letra mayúscula',
-    'Al menos una letra minúscula',
-    'Al menos un número',
+    "Al menos 8 caracteres",
+    "Al menos una letra mayúscula",
+    "Al menos una letra minúscula",
+    "Al menos un número",
   ];
   //___________________________________________________________________________________________
 
-
   const handleLogin = async (event) => {
-
     event.preventDefault();
-    setCargando(true);// activa la ventana para mostrar 'cargando...'
-      try {
-         const response = await registro(firstName, lastName, email, password)
-            console.log(response)
-            
-      } catch (error) {
-          console.error('Error en la solicitud:', error);
-          setAlertMessage('Error en las credenciales. Vefifica e intentalo nuevamente');
-          setShowAlert(true); // Mostrar la alerta
-          // Reiniciar el formulario
-          setFirstName('');
-          setLastName('');
-          setEmail('');
-          setPassword('');
-     }// codigo de ventana 'cargando...'
-     finally {
+    setCargando(true); // activa la ventana para mostrar 'cargando...'
+    try {
+      const response = await registro(firstName, lastName, email, password);
+      console.log(response);
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+      setAlertMessage(
+        "Error en las credenciales. Vefifica e intentalo nuevamente"
+      );
+      setShowAlert(true); // Mostrar la alerta
+      // Reiniciar el formulario
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+    } finally {
+      // codigo de ventana 'cargando...'
       // Independientemente del resultado, deja de mostrar el estado de carga
       setCargando(false);
     }
   };
-
-  
 
   return (
     <div className="signup">
@@ -61,42 +63,86 @@ const LoginUser = () => {
       {showAlert && (
         <div className="floating-alert2">
           <span>{alertMessage}</span>
-          <button onClick={()=> setShowAlert(false)}>Cerrar</button>
+          <button onClick={() => setShowAlert(false)}>Cerrar</button>
         </div>
       )}
       <form onSubmit={handleLogin}>
         <div className="container">
-          <h1>SIGN UP</h1>
-          <input type="text" placeholder="firstName" value={firstName} 
-            onChange={(e) => setFirstName(e.target.value)} required/>
-
-          <input type="text" placeholder="LastName" value={lastName} 
-            onChange={(e) => setLastName(e.target.value)} required/>
-          
-          <input type="email" placeholder="Email" value={email} 
-            onChange={(e) => setEmail(e.target.value)} required/>
-          
-          <input type="password" placeholder="Password" value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            onFocus={() => setShowPasswordRestrictions(true)}
-            onBlur={() => setShowPasswordRestrictions(false)}  required/>
-          
-            {showPasswordRestrictions && (
-              <div className="textRestriccion">
-                {passwordRestrictions.map((restriction, index) => (
-                  <p key={index}>{restriction}</p>
-                ))}
-              </div>
-            )}
-
-            <button type='submit'>Registrar</button>
-
-            <span>Ya tienes cuenta!  <Link to='/login'>Ingresa</Link></span>
-
+          <header className="content-img">
+            <img src={img} />
+          </header>
+          <div className="content-inputs">
+            <fieldset className="inputs">
+              <span className="icons">
+                <KeyboardAltIcon />
+              </span>
+              <input
+                type="text"
+                placeholder="Nombres..."
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </fieldset>
+            <fieldset className="inputs">
+              <span className="icons">
+                <KeyboardAltIcon />
+              </span>
+              <input
+                type="text"
+                placeholder="Apellidos..."
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)} 
+                required
+              />
+            </fieldset>
+            <fieldset className="inputs">
+              <span className="icons">
+                <PersonIcon />
+              </span>
+              <input
+                type="text"
+                placeholder="Usuario"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </fieldset>
+            <fieldset className="inputs">
+              <span className="icons">
+                <VpnKeyIcon />
+              </span>
+              <input
+                type="password"
+                placeholder="**********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onFocus={() => setShowPasswordRestrictions(true)}
+                onBlur={() => setShowPasswordRestrictions(false)}
+                required
+              />
+              {showPasswordRestrictions && (
+                <div className="textRestriccion">
+                  {passwordRestrictions.map((restriction, index) => (
+                    <p key={index}>{restriction}</p>
+                  ))}
+                </div>
+              )}
+            </fieldset>
+            <span className="registro">
+              Ya tienes cuenta!{" "}
+              <Link to="/login" style={{ marginLeft: "5px" }}>
+                Ingresa
+              </Link>
+            </span>
+          </div>
         </div>
+        <footer className="btn-iniciar">
+          <button type="submit">Registrar</button>
+        </footer>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginUser
+export default LoginUser;
